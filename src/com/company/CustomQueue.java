0d790b84e4fd052c9360843338c25b81b7764c0f
gevor <gevorg.aghekyan94@gmail.com>
2021-03-14 21:40:18 +0400

    Custom_Queue

--- src/com/company/CustomQueue.java
+++ src/com/company/CustomQueue.java
@@ -1,4 +1,48 @@
 package com.company;
 
-public class Queue {
+import java.util.Arrays;
+
+public class CustomQueue {
+
+    private Integer[] array;
+    private int count;
+
+    public CustomQueue() {
+        this.array = new Integer[10];
+        this.count = 0;
+    }
+
+    public void add(int num) {
+        if (this.count >= this.array.length) {
+            Integer[] copyArray = new Integer[this.array.length + 10];
+            for (int i = 0; i < this.array.length; i++) {
+                copyArray[i] = this.array[i];
+            }
+            this.array = copyArray;
+        }
+        this.array[count] = num;
+        count++;
+    }
+
+    public Integer poll() {
+        return this.array[count - 1];
+    }
+
+    public Integer peek() {
+        Integer last = this.array[count - 1];
+        this.array[count - 1] = null;
+        count--;
+        return last;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomQueue{" +
+                "array=" + Arrays.toString(array) +
+                '}';
+    }
 }
